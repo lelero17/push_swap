2b1365522e79769b483354f28f45772693f7c1c1
lelero17 <leandro-emmerling@web.de>
2026-01-15 16:39:07 +0100

    Implement stack utility functions

---
+++ stack_utils.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lemmerli <lemmerli@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/15 10:21:15 by lemmerli          #+#    #+#             */
+/*   Updated: 2026/01/15 15:31:38 by lemmerli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*stack_new(int value)
+{
+	t_stack	*new;
+
+	new = malloc(sizeof(t_stack));
+	if (!new)
+		return (NULL);
+	new->value = value;
+	new->next = NULL;
+	return (new);
+}
+
+void	stack_add_back(t_stack **stack, t_stack *new)
+{
+	t_stack	*tmp;
+
+	if (*stack == NULL)
+	{
+		*stack = new;
+		return;
+	}
+	tmp = *stack;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+}
+
+void	stack_free(t_stack **stack)
+{
+	t_stack	*tmp;
+
+	while(*stack != NULL)
+	{
+		tmp = *stack;
+		*stack = (*stack)->next;
+		free(tmp);
+	}
+}
